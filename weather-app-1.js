5c08121b9ab2ab0480650768e2af6416a67c675a
Ethan Bevan <ethanbevan@Ethans-Mac-mini.local>
2025-08-19 14:36:23 +0200

    updates sizing etc

--- weather-app-1.js
+++ weather-app-1.js
@@ -1,8 +1,8 @@
-
 console.log("Hello");
 
 const apiKey = "65211a8acdd183a34d3df5ab5b2f942a";
-const apiURL = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+const apiURL =
+  "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
 
 const searchBox = document.querySelector(".search input");
 const searchBtn = document.querySelector(".search button");
@@ -11,204 +11,176 @@ const weatherIcons = document.querySelector(".weatherIcons");
 
 const backgroundApp = document.getElementById("BackgroundApp");
 
-
-
 async function checkWeather(city) {
+  const response = await fetch(apiURL + city + `&appid=${apiKey}`);
 
-    const response = await fetch(apiURL + city + `&appid=${apiKey}`);
-   
-   if (response.status == 404 || !city ) {
-
+  if (response.status == 404 || !city) {
     document.querySelector(".cityName").innerHTML = "Spelling error";
     document.querySelector(".errorMessage").style.display = "block";
     document.querySelector(".cityTemp").innerHTML = "";
-    document.querySelector(".errorMessage").innerHTML = "Try again with correct city name";
+    document.querySelector(".errorMessage").innerHTML =
+      "Try again with correct city name";
     document.querySelector(".errorMessage").style.color = "#ff8d8d";
+  } else var data = await response.json();
+  document.querySelector(".cityTemp").style.display = "block";
 
-   } else
-   
-    var data = await response.json();
-    document.querySelector(".cityTemp").style.display = "block";
-   
+  document.querySelector(".cityName").innerHTML = data.name;
+  document.querySelector(".cityTemp").innerHTML =
+    Math.round(data.main.temp) + " °C";
+  document.querySelector(".cityFeelsLike").innerHTML =
+    Math.round(data.main.feels_like) + " °C";
+  document.querySelector(".cityType").innerHTML = data.weather[0].main;
+  document.querySelector(".cityMax").innerHTML =
+    Math.round(data.main.temp_max) + " °C";
 
+  document.querySelector(".main-weather-app h3").style.display = "none";
 
-    document.querySelector(".cityName").innerHTML = data.name;
-    document.querySelector(".cityTemp").innerHTML = Math.round(data.main.temp) + " °C";
-    document.querySelector(".cityFeelsLike").innerHTML = Math.round(data.main.feels_like) + " °C";
-    document.querySelector(".cityType").innerHTML = data.weather[0].main;
-    document.querySelector(".cityMax").innerHTML = Math.round(data.main.temp_max) + " °C";
+  if (data.weather[0].main == "Clouds") {
+    weatherIcons.src = "./weather-icons/Clouds.png";
+    backgroundApp.className = "";
+    backgroundApp.className = "gradientBackgroundClouds";
+  } else if (data.weather[0].main == "Drizzle") {
+    weatherIcons.src = "./weather-icons/Drizzle.png";
 
-    document.querySelector(".main-weather-app h3").style.display = "none";
+    backgroundApp.className = "";
+    backgroundApp.className = "gradientBackgroundDrizzle";
+  } else if (data.weather[0].main == "Rain") {
+    weatherIcons.src = "./weather-icons/Rain.png";
 
+    backgroundApp.className = "";
+    backgroundApp.className = "gradientBackgroundRain";
+  } else if (data.weather[0].main == "Snow") {
+    weatherIcons.src = "./weather-icons/Snow.png";
+    weatherIcons.style.height = "70px";
 
-    if(data.weather[0].main == "Clouds" ) {
-         weatherIcons.src = "./weather-icons/Clouds.png";
-         backgroundApp.className = "";
-        backgroundApp.className = "gradientBackgroundClouds";
-     } 
+    backgroundApp.className = "";
+    backgroundApp.className = "gradientBackgroundSnow";
+  } else if (data.weather[0].main == "Atmosphere") {
+    weatherIcons.src = "./weather-icons/Atmosphere.png";
 
-    else if(data.weather[0].main == "Drizzle" ) {
-        weatherIcons.src = "./weather-icons/Drizzle.png";
-        
-        backgroundApp.className = "";
-        backgroundApp.className = "gradientBackgroundDrizzle";
-    } 
+    backgroundApp.className = "";
+    backgroundApp.className = "gradientBackgroundAtmosphere";
+  } else if (data.weather[0].main == "Clear") {
+    weatherIcons.src = "./weather-icons/Clear.png";
+    weatherIcons.style.height = "70px";
 
-    else if(data.weather[0].main == "Rain" ) {
-        weatherIcons.src = "./weather-icons/Rain.png";
+    backgroundApp.className = "";
+    backgroundApp.className = "gradientBackgroundClear";
+  } else if (data.weather[0].main == "Thunderstorm") {
+    weatherIcons.src = "./weather-icons/Thunderstorm.png";
 
-        backgroundApp.className = "";
-        backgroundApp.className = "gradientBackgroundRain";
-    } 
-
-    else if(data.weather[0].main == "Snow" ) {
-        weatherIcons.src = "./weather-icons/Snow.png";
-        weatherIcons.style.height = "85px";
-       
-        backgroundApp.className = "";
-        backgroundApp.className = "gradientBackgroundSnow";
-    } 
-
-    else if(data.weather[0].main == "Atmosphere" ) {
-        weatherIcons.src = "./weather-icons/Atmosphere.png";
-
-        backgroundApp.className = "";
-        backgroundApp.className = "gradientBackgroundAtmosphere";
-    } 
-
-    else if(data.weather[0].main == "Clear" ) {
-        weatherIcons.src = "./weather-icons/Clear.png";
-        weatherIcons.style.height = "95px";
-
-        backgroundApp.className = "";
-        backgroundApp.className = "gradientBackgroundClear";
-    } 
-
-    else if (data.weather[0].main == "Thunderstorm" ) {
-        weatherIcons.src = "./weather-icons/Thunderstorm.png";
-
-        backgroundApp.className = "";
-        backgroundApp.className = "gradientBackgroundThunderstorm";
-    } 
-
+    backgroundApp.className = "";
+    backgroundApp.className = "gradientBackgroundThunderstorm";
+  }
 }
 
-
-
-
 function onClickorEnter(event) {
-
-    if (event.type === "click" || event.key === "Enter") {
+  if (event.type === "click" || event.key === "Enter") {
     checkWeather(searchBox.value);
-
-}};
-
+  }
+}
 
 searchBtn.addEventListener("click", onClickorEnter);
 searchBox.addEventListener("keydown", onClickorEnter);
 
-  
-
 // Lsit of cities
 
 const cityData = {
-    "cities": [
-        "New York City",
-        "Los Angeles",
-        "London",
-        "Tokyo",
-        "Paris",
-        "Sydney",
-        "Berlin",
-        "São Paulo",
-        "Mumbai",
-        "Moscow",
-        "Shanghai",
-        "Toronto",
-        "Mexico City",
-        "Cairo",
-        "Madrid",
-        "Dubai",
-        "Rome",
-        "Beijing",
-        "Istanbul",
-        "Buenos Aires",
-        "Rio de Janeiro",
-        "Seoul",
-        "Los Angeles",
-        "San Francisco",
-        "Lagos",
-        "Kuala Lumpur",
-        "Hong Kong",
-        "Jakarta",
-        "Cape Town",
-        "Lagos",
-        "Oslo",
-        "Chicago",
-        "São Paulo",
-        "Dubai",
-        "Berlin",
-        "Bangkok",
-        "Paris",
-        "Singapore",
-        "Lagos",
-        "Miami",
-        "Cape Town",
-        "Caracas",
-        "Lima",
-        "Sydney",
-        "Tokyo",
-        "Seattle",
-        "Madrid",
-        "Vienna",
-        "Dallas",
-        "Berlin",
-        "San Diego",
-        "Los Angeles",
-        "Detroit",
-        "Dallas",
-        "Copenhagen",
-        "Athens",
-        "Stockholm",
-        "Tehran",
-        "Zurich",
-        "Helsinki",
-        "Barcelona",
-        "Mexico City",
-        "Warsaw",
-        "Los Angeles",
-        "Riyadh",
-        "Milan",
-        "Bucharest",
-        "Dubai",
-        "Lagos",
-        "Bengaluru",
-        "Lagos",
-        "Lagos",
-        "Bangladesh",
-        "Lagos",
-        "Auckland",
-        "Frankfurt",
-        "Lisbon",
-        "Buenos Aires",
-        "Montreal",
-        "Rio de Janeiro",
-        "Manila",
-        "Durban",
-        "Osaka",
-        "St. Petersburg",
-        "Jakarta",
-        "Cape Town",
-        "Lagos",
-        "Calcutta",
-        "Milan",
-        "Bucharest",
-        "Vancouver",
-        "Vienna"
-    ]
+  cities: [
+    "New York City",
+    "Los Angeles",
+    "London",
+    "Tokyo",
+    "Paris",
+    "Sydney",
+    "Berlin",
+    "São Paulo",
+    "Mumbai",
+    "Moscow",
+    "Shanghai",
+    "Toronto",
+    "Mexico City",
+    "Cairo",
+    "Madrid",
+    "Dubai",
+    "Rome",
+    "Beijing",
+    "Istanbul",
+    "Buenos Aires",
+    "Rio de Janeiro",
+    "Seoul",
+    "Los Angeles",
+    "San Francisco",
+    "Lagos",
+    "Kuala Lumpur",
+    "Hong Kong",
+    "Jakarta",
+    "Cape Town",
+    "Lagos",
+    "Oslo",
+    "Chicago",
+    "São Paulo",
+    "Dubai",
+    "Berlin",
+    "Bangkok",
+    "Paris",
+    "Singapore",
+    "Lagos",
+    "Miami",
+    "Cape Town",
+    "Caracas",
+    "Lima",
+    "Sydney",
+    "Tokyo",
+    "Seattle",
+    "Madrid",
+    "Vienna",
+    "Dallas",
+    "Berlin",
+    "San Diego",
+    "Los Angeles",
+    "Detroit",
+    "Dallas",
+    "Copenhagen",
+    "Athens",
+    "Stockholm",
+    "Tehran",
+    "Zurich",
+    "Helsinki",
+    "Barcelona",
+    "Mexico City",
+    "Warsaw",
+    "Los Angeles",
+    "Riyadh",
+    "Milan",
+    "Bucharest",
+    "Dubai",
+    "Lagos",
+    "Bengaluru",
+    "Lagos",
+    "Lagos",
+    "Bangladesh",
+    "Lagos",
+    "Auckland",
+    "Frankfurt",
+    "Lisbon",
+    "Buenos Aires",
+    "Montreal",
+    "Rio de Janeiro",
+    "Manila",
+    "Durban",
+    "Osaka",
+    "St. Petersburg",
+    "Jakarta",
+    "Cape Town",
+    "Lagos",
+    "Calcutta",
+    "Milan",
+    "Bucharest",
+    "Vancouver",
+    "Vienna",
+  ],
 };
-
-
 
 window.onload = randomCityGenerator;
 
@@ -217,33 +189,27 @@ window.onload = randomCityGenerator;
 document.getElementById("cityButtonShuffle").onclick = randomCityGenerator;
 
 function randomCityGenerator() {
+  const randomCityNumber = Math.floor(Math.random() * 92);
 
-    const randomCityNumber = Math.floor(Math.random() * 92);
-  
-    const cityName = cityData.cities[randomCityNumber];
-  
-    document.getElementById("cityButton").textContent = cityName
+  const cityName = cityData.cities[randomCityNumber];
 
-    document.getElementById("cityButton").style.color = "rgb(110, 110, 110)";
+  document.getElementById("cityButton").textContent = cityName;
 
-    }
+  document.getElementById("cityButton").style.color = "rgb(110, 110, 110)";
+}
 
 // Display city
 
 document.getElementById("cityButton").onclick = displayRandomCity;
-  
-
 
 function displayRandomCity() {
+  document.getElementById("cityButton").style.color = "black";
 
-    document.getElementById("cityButton").style.color = "black";
-    
-    const currentRandomCity = document.getElementById("cityButton").innerHTML;
+  const currentRandomCity = document.getElementById("cityButton").innerHTML;
 
-    console.log(currentRandomCity);
-    
-    document.getElementById("weatherSearch").value = "";
+  console.log(currentRandomCity);
 
-    checkWeather(currentRandomCity) 
+  document.getElementById("weatherSearch").value = "";
 
+  checkWeather(currentRandomCity);
 }
